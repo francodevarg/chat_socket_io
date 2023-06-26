0135a1066743dbbdffefb9965de77b3a993b0afb
Franco <juanfranconarvaez@gmail.com>
2023-06-25 22:50:59 -0300

    Init project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pre requisites
+
+    "cors": "^2.8.5",
+    "express": "^4.18.2",
+    "morgan": "^1.10.0",
+    "socket.io": "^4.7.0"
+
+NOde v20.3.1
---
+++ server/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import http from "http";
+import {Server as SocketServer} from 'socket.io'
+
+const app = express();
+const server = http.createServer(app);
+const io = new SocketServer(server);
+
+io.on("connection", (socket) => {
+    console.log(socket.id);
+    socket.on("message", (body) => {
+      socket.broadcast.emit("message", {
+        body,
+        from: socket.id.slice(8),
+      });
+    });
+  });
+
+app.listen(3000);
+console.log("Server on port ", 3000)
